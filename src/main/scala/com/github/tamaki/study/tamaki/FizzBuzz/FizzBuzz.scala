cd379bc1d7642b137170e5aa6625f7347bf3b3cd
tomoki-koga <tomoki.koga@bizreach.co.jp>
2015-02-09 11:35:26 +0900

    FizzBuzz実装

--- src/main/scala/com/github/tamaki/study/tamaki/FizzBuzz/FizzBuzz.scala
+++ src/main/scala/com/github/tamaki/study/tamaki/FizzBuzz/FizzBuzz.scala
@@ -1,10 +1,19 @@
 package com.github.tamaki.study.tamaki.FizzBuzz
 
 /**
- * Created by tamaki on 2015/02/08.
+ * Created by tomoki.koga@bizreach.co.jp on 2015/02/08.
  */
 object FizzBuzz {
   def main(args: Array[String]): Unit = {
+    (1 to 100).foreach(sorting)
+  }
 
+  def sorting(num: Int): Unit = {
+    (num % 3, num % 5) match {
+      case (0, 0) => println("FizzBuzz")
+      case (0, _) => println("Fizz")
+      case (_, 0) => println("Buzz")
+      case _ => println(num)
+    }
   }
 }
