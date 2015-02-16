d9063730e8c3143bd9f659832f10d78b2ec0d046
tomoki-koga <tomoki.koga@bizreach.co.jp>
2015-02-16 10:39:34 +0900

    2の倍数を除外したFizzBuzz

--- src/main/scala/com/github/tamaki/study/tamaki/FizzBuzz/FizzBuzz.scala
+++ src/main/scala/com/github/tamaki/study/tamaki/FizzBuzz/FizzBuzz.scala
@@ -5,15 +5,24 @@ package com.github.tamaki.study.tamaki.FizzBuzz
  */
 object FizzBuzz {
   def main(args: Array[String]): Unit = {
-    (1 to 100).foreach(sorting)
+
+    // 2の倍数を除去する
+    (1 to 100).filter(_ % 2 != 0).map {
+      case n if n % 15 == 0 => "FizzBuzz"
+      case n if n %  3 == 0 => "Fizz"
+      case n if n %  5 == 0 => "Buzz"
+      case n => n.toString
+    }.foreach(println)
+
+//  (1 to 100).map(sorting).foreach(println)
   }
 
-  def sorting(num: Int): Unit = {
+  def sorting(num: Int): String = {
     (num % 3, num % 5) match {
-      case (0, 0) => println("FizzBuzz")
-      case (0, _) => println("Fizz")
-      case (_, 0) => println("Buzz")
-      case _ => println(num)
+      case (0, 0) => "FizzBuzz"
+      case (0, _) => "Fizz"
+      case (_, 0) => "Buzz"
+      case _ => num.toString
     }
   }
 }
